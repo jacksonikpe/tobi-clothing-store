bccd01bf88377481b8caaf398efbf0a6a8966f2a
Jackson Ikpe <Jikpe@wragbysolutions.com>
2022-11-20 12:19:19 +0100

    added auth with email and password

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,50 +1,62 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCCA8S0-xpWh3A8mada37_C3PrXGnR-ScA",
-    authDomain: "tobi-clothing-db.firebaseapp.com",
-    projectId: "tobi-clothing-db",
-    storageBucket: "tobi-clothing-db.appspot.com",
-    messagingSenderId: "489732505823",
-    appId: "1:489732505823:web:d928eeeed7cc02564fe96a"
+  apiKey: "AIzaSyCCA8S0-xpWh3A8mada37_C3PrXGnR-ScA",
+  authDomain: "tobi-clothing-db.firebaseapp.com",
+  projectId: "tobi-clothing-db",
+  storageBucket: "tobi-clothing-db.appspot.com",
+  messagingSenderId: "489732505823",
+  appId: "1:489732505823:web:d928eeeed7cc02564fe96a",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider()
+const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-    prompt: "select_account"
-})
+  prompt: "select_account",
+});
 
-export const auth = getAuth()
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
-export const db = getFirestore() 
+export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
-    const userDocRef = doc(db, 'users', userAuth.uid)
-    
-    console.log(userDocRef)
-    
-    const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot)
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformaiton = {}
+) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
 
-    if(!userSnapshot.exists()) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
 
-        try {
-            await setDoc(userDocRef, {
-                displayName,
-                email,
-                createdAt
-            })
-        } catch (error){
-            console.log('error creating the user', error.message)
-        }
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
     }
-    return userDocRef;
-}
+  }
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
