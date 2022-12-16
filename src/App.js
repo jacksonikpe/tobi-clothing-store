7646ecb961b8da5218040a11444a14e5cce0693d
Jackson Ikpe <Jikpe@wragbysolutions.com>
2022-12-16 12:52:49 +0100

    added and styled sign-in form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
-import './cartegories.style.scss'
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import "./cartegories.style.scss";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
 
-const App = () => { 
+const App = () => {
   const Shop = () => {
-   return <h1>This is a shop</h1>
-  }
+    return <h1>This is a shop</h1>;
+  };
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} /> 
-        <Route path='/shop' element={<Shop />} />
-        <Route path='/sign-in' element={<SignIn />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
-}
+};
 
 export default App;
