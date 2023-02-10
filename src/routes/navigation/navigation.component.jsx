706dbacb8eff05273fd1091dc0e2ade95472a14d
Jackson Ikpe <Jikpe@wragbysolutions.com>
2023-02-10 09:20:38 +0100

    set user context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
 
 import "./navigation.styles.scss";
+import { UserContext } from "../../contexts/user.context";
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  console.log(currentUser, "Navigation");
   return (
     <>
       <div className="navigation">
